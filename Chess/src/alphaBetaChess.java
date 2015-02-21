3850f9b2dff194434935e7e2e489113684902b0c
bradtracy <bradktracy@gmail.com>
2015-02-21 10:31:51 -0500

    [Updates through video 6]

--- Chess/src/alphaBetaChess.java
+++ Chess/src/alphaBetaChess.java
@@ -1,5 +1,14 @@
 import javax.swing.*;
 public class alphaBetaChess {
+	static String chessBoard[][]={
+        {"r","k","b","q","a","b","k","r"},
+        {"p","p","p","p","p","p","p","p"},
+        {" "," "," "," "," "," ","P"," "},
+        {" "," "," "," "," "," "," "," "},
+        {" "," "," "," "," "," "," "," "},
+        {" "," "," "," "," "," "," "," "},
+        {"P","P","P","P","P","P","P","P"},
+        {"R","K","B","Q","A","B","K","R"}};
     public static void main(String[] args) {
         JFrame f=new JFrame("B & B Chess");
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -7,5 +16,8 @@ public class alphaBetaChess {
         f.add(ui);
         f.setSize(500, 500);
         f.setVisible(true);
+    }
+    public static String possibleMoves() {
+        return "";//x1,y1,x2,yx,captured piece
     }
 }
